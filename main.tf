724545cbf76a8eec76e69a01246365fa581dff08
Venkata Mutyala <venkata@venkatamutyala.com>
2023-06-13 06:35:23 +0000

    feat: addinig remaining providers and their respective versions

--- main.tf
+++ main.tf
@@ -2,8 +2,56 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.66.0"
+      version = "4.67.0"
     }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "4.5.0"
+    }
+
+    google = {
+      source  = "hashicorp/google"
+      version = "4.64.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "2.1.0"
+    }
+
+    vault = {
+      source  = "hashicorp/vault"
+      version = "3.15.2"
+    }
+
+    opsgenie = {
+      source  = "opsgenie/opsgenie"
+      version = "0.6.20"
+    }
+
+    vaultoperator = {
+      source  = "rickardgranberg/vaultoperator"
+      version = "0.1.8"
+    }
+
+    github = {
+      source  = "github/integrations/github"
+      version = "5.25.0"
+    }
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = "0.38.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
+
+    time = {
+      source  = "hashicorp/time"
+      version = "0.9.1"
+    }
+
   }
-  
+
 }
