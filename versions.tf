feef563547e1376ea8d375c49dfb4bdda4caaabd
fernandoataoldotcom <antonio.rodriguez@glueops.dev>
2023-08-21 11:17:13 -0700

    chore: update aws provider 5.12.0 > 5.13.1

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
     
     aws = {
       source  = "hashicorp/aws"
-      version = "5.12.0"
+      version = "5.13.1"
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
