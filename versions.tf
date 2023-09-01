9df58459732a1f8c3e92bc0d7e5fa99f5974f8f8
Venkata Mutyala <venkata@venkatamutyala.com>
2023-09-01 14:20:45 -0700

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
     
     aws = {
       source  = "hashicorp/aws"
-      version = "5.13.1"
+      version = "5.14.0"
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
