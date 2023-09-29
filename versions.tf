ec819dda175bcd50743400a088f3095c0176024c
Venkata Mutyala <venkata@venkatamutyala.com>
2023-09-28 19:42:09 -0700

    feat: update AWS provider to 5.19.0 (#21)

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
     
     aws = {
       source  = "hashicorp/aws"
-      version = "5.18.1"
+      version = "5.19.0"
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
